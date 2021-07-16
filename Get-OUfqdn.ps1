54496351800e50ea6e49f4fdd2a3de422433f482
Luke Leigh <luke.leigh@gmail.com>
2021-07-16 23:13:38 +0100

    Update Get-OUfqdn.ps1

--- Get-OUfqdn.ps1
+++ Get-OUfqdn.ps1
@@ -8,14 +8,27 @@ function Get-OUfqdn {
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
         [OutputType([string])]
-        [ArgumentCompleter( {
-                $baseOU = (Get-ADDomain).DistinguishedName
-                $OUs = Get-ADOrganizationalUnit -Filter * -SearchScope SubTree -SearchBase $baseOU | Sort-Object -Property DistinguishedName
-                foreach ($OU in $OUs) { 
-                    ($OU).DistinguishedName
-                }
-            } ) ]
         [string]
-        $SelectOU
+        $Department
     )
+
+    $baseOU = (Get-ADDomain).DistinguishedName
+    $OUs = Get-ADOrganizationalUnit -Filter * -SearchScope SubTree -SearchBase $baseOU | Sort-Object -Property DistinguishedName
+    $UserOU = $OUs | Where-Object { ( $_.DistinguishedName -Like "*$Department*" ) -and ( $_.DistinguishedName -Like '*users*' ) } | Select-Object -Property DistinguishedName
+    $UserOU.DistinguishedName
+
 }
+
+
+# [ArgumentCompleter( {
+#     $baseOU = (Get-ADDomain).DistinguishedName
+#     $OUs = Get-ADOrganizationalUnit -Filter * -SearchScope SubTree -SearchBase $baseOU | Sort-Object -Property DistinguishedName
+#     $OUs | Where-Object { ( $_.DistinguishedName -Like $SelectOU ) -and ( $_.DistinguishedName -Like '*users*' ) } | Select-Object -Property DistinguishedName
+# } ) ]
+
+
+# $Department = Read-Host -Prompt "Enter Department String"
+# $baseOU = (Get-ADDomain).DistinguishedName
+# $OUs = Get-ADOrganizationalUnit -Filter * -SearchScope SubTree -SearchBase $baseOU | Sort-Object -Property DistinguishedName
+# $UserOU = $OUs | Where-Object { ( $_.DistinguishedName -Like "*$Department*" ) -and ( $_.DistinguishedName -Like '*users*' ) } | Select-Object -Property DistinguishedName
+# $UserOU.DistinguishedName
